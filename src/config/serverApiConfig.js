a1d851b3046c7dd6400eea36be302e396dd3ce75
Lakshmi <lakshmi@Lakshmis-MacBook-Air.local>
2024-09-05 16:07:11 +0530

    url edited

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -1,19 +1,19 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://localhost:8000/api/';
+    : 'http://si.nividhi.com:5000/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
-    : 'http://localhost:8000/';
+    : 'http://si.nividhi.com:5000/';
 
 export const WEBSITE_URL = import.meta.env.PROD
-  ? 'http://cloud.idurarapp.com/'
-  : 'http://localhost:8000/';
+  ? 'http://si.nividhi.com:5000/'
+  : 'http://si.nividhi.com:5000/';
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'http://localhost:8000/download/';
+    : 'http://si.nividhi.com:5000/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
 
 export const FILE_BASE_URL = import.meta.env.VITE_FILE_BASE_URL;
